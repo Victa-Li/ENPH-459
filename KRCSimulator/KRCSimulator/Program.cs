18616e446f3eff468ab95857b6b37c0ec0248e35
Ivan Zinin <ivan.z.inventor@gmail.com>
2017-03-19 13:16:02 -0700

    Initial commit

---
+++ KRCSimulator/KRCSimulator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Net;
+using System.Net.Sockets;
+
+namespace KRCSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
