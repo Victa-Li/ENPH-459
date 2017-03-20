51e90b71551ad2b93dbbcbc7fe06fc5b4a2e0e2d
Ivan Zinin <ivan.z.inventor@gmail.com>
2017-03-19 17:10:03 -0700

    Updated simulator to use XML fomatting

--- KRCSimulator/KRCSimulator/Program.cs
+++ KRCSimulator/KRCSimulator/Program.cs
@@ -1,18 +1,24 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Net;
 using System.Net.Sockets;
+using System.IO;
 
 namespace KRCSimulator
 {
+    public class Utf8StringWriter : StringWriter
+    {
+        public override Encoding Encoding
+        {
+            get { return Encoding.UTF8; }
+        }
+    }
+
     class Program
     {
         private const int targetPort = 59152;
-        private const String targetAddress = "206.12.45.26"; //"192.168.2.100";
+        private const String targetAddress = "192.168.2.200"; //"192.168.2.100";
 
         private static double X = 331.7, Y = -1.2, Z = 852.0, A = -90.0, B = 0.9, C = -90.0;
         private static double sineValue;
@@ -23,7 +29,6 @@ namespace KRCSimulator
         static void Main(string[] args)
         {
             Boolean done = false;
-            Boolean exception_thrown = false;
 
             Socket sending_socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
             IPAddress send_to_address = IPAddress.Parse(targetAddress);
@@ -48,9 +53,10 @@ namespace KRCSimulator
                 {
                     // Update internal values:
                     timestamp = DateTime.Now.Second;
-                    sineValue = Math.Sin(DateTime.Now.Millisecond * Math.PI / 2000);
+                    sineValue = Math.Sin(DateTime.Now.Ticks * Math.PI / 20000000);
 
                     // Sample send message
+                    /*
                     string replyMessage = "< Rob Type =\"KUKA\">" +
                         "< RIst X =\"" + X + "\" Y=\"" + Y + "\" Z=\"" + Z + "\" A=\"" + A + "\" B=\"" + B + "\" C=\"" + C + "\"/>" +
                         "< RSol X =\"" + X + "\" Y=\"" + Y + "\" Z=\"" + Z + "\" A=\"" + A + "\" B=\"" + B + "\" C=\"" + C + "\"/>" +
@@ -60,7 +66,61 @@ namespace KRCSimulator
                         "< SineSource > " + sineValue + " </ SineSource >" +
                         "< IPOC > " + timestamp + " </ IPOC >" +
                         "</ Rob >";
+                    */
+                    //
+                    string replyMessage = "";
 
+                    XmlWriterSettings Settings = new XmlWriterSettings();
+                    Settings.OmitXmlDeclaration = true;
+                    Settings.ConformanceLevel = ConformanceLevel.Fragment;
+                    Settings.NewLineOnAttributes = true;
+
+                    string position = "X =\"" + X + "\" Y=\"" + Y + "\" Z=\"" + Z + "\" A=\"" + A + "\" B=\"" + B + "\" C=\"" + C + "\"";
+                    using (TextWriter textWriter = new Utf8StringWriter())
+                    {
+                        using (XmlWriter xmlWriter = XmlWriter.Create(textWriter, Settings))
+                        {
+                            xmlWriter.WriteStartElement("Rob");
+                            xmlWriter.WriteAttributeString(null, "Type", null, "KUKA");
+                            xmlWriter.WriteStartElement("RIst");
+                            xmlWriter.WriteAttributeString(null, "X", null, X.ToString());
+                            xmlWriter.WriteAttributeString(null, "Y", null, Y.ToString());
+                            xmlWriter.WriteAttributeString(null, "Z", null, Z.ToString());
+                            xmlWriter.WriteAttributeString(null, "A", null, A.ToString());
+                            xmlWriter.WriteAttributeString(null, "B", null, B.ToString());
+                            xmlWriter.WriteAttributeString(null, "C", null, C.ToString());
+                            xmlWriter.WriteEndElement();
+                            xmlWriter.WriteStartElement("RSol");
+                            xmlWriter.WriteAttributeString(null, "X", null, X.ToString());
+                            xmlWriter.WriteAttributeString(null, "Y", null, Y.ToString());
+                            xmlWriter.WriteAttributeString(null, "Z", null, Z.ToString());
+                            xmlWriter.WriteAttributeString(null, "A", null, A.ToString());
+                            xmlWriter.WriteAttributeString(null, "B", null, B.ToString());
+                            xmlWriter.WriteAttributeString(null, "C", null, C.ToString());
+                            xmlWriter.WriteEndElement();
+                            xmlWriter.WriteStartElement("Delay");
+                            xmlWriter.WriteAttributeString(null, "D", null, delay.ToString());
+                            xmlWriter.WriteEndElement();
+                            xmlWriter.WriteStartElement("Tech");
+                            xmlWriter.WriteAttributeString(null, "C11", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C12", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C13", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C14", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C15", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C16", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C17", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C18", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C19", null, "0.0");
+                            xmlWriter.WriteAttributeString(null, "C110", null, "0.0");
+                            xmlWriter.WriteEndElement();
+                            xmlWriter.WriteElementString("TestOutput", testOutput.ToString());
+                            xmlWriter.WriteElementString("SineSource", string.Format("{0:0.00}", sineValue) );
+                            xmlWriter.WriteElementString("IPOC", timestamp.ToString());
+
+                            xmlWriter.WriteEndElement();
+                        }
+                        replyMessage = textWriter.ToString();
+                    }
 
                     byte[] send_buffer = Encoding.ASCII.GetBytes(replyMessage);
                     
@@ -72,7 +132,6 @@ namespace KRCSimulator
                     }
                     catch (Exception send_exception)
                     {
-                        exception_thrown = true;
                         Console.WriteLine(send_exception.ToString());
                         break;
                     }
@@ -85,6 +144,18 @@ namespace KRCSimulator
                         Console.WriteLine("Received from {0}", receiving_end_point.ToString());
                         received_data = Encoding.ASCII.GetString(receive_byte_array, 0, receive_byte_array.Length);
                         Console.WriteLine("data follows \n\n{0}\n", received_data);
+
+                        XmlDocument doc = new XmlDocument();
+                        doc.LoadXml(received_data);
+                        XmlNode newPos = doc.DocumentElement.SelectSingleNode("/Sen/RKorr");
+                        X = Convert.ToDouble(newPos.Attributes["X"].Value);
+                        Y = Convert.ToDouble(newPos.Attributes["Y"].Value);
+                        Z = Convert.ToDouble(newPos.Attributes["Z"].Value);
+                        A = Convert.ToDouble(newPos.Attributes["A"].Value);
+                        B = Convert.ToDouble(newPos.Attributes["B"].Value);
+                        C = Convert.ToDouble(newPos.Attributes["C"].Value);
+                        XmlNode newTestOutput = doc.DocumentElement.SelectSingleNode("/Sen/TestOutput");
+                        testOutput = Convert.ToInt32(newTestOutput.Value);
                     }
                     catch (Exception e)
                     {
@@ -92,7 +163,7 @@ namespace KRCSimulator
                         break;
                     }
                 }
-                System.Threading.Thread.Sleep(1000);
+                //System.Threading.Thread.Sleep(500);
                 count--;
             } // end of while (!done)
 
